3577dddb1fc7780383c319ddec20301bbd73ad54
Eduardo San Martin Morote <posva13@gmail.com>
2024-08-21 11:49:45 +0200

    deps and style

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    VueRouter({
+      dts: 'src/typed-router.d.ts',
+    }),
+    Vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
